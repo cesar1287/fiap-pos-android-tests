c8dbcf7ed3f863409b8a0318f91f1ab0d4910a52
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-04-15 10:59:28 -0300

    improving game tests

--- app/src/test/java/com/github/cesar1287/jogodavelha/model/GameTest.kt
+++ app/src/test/java/com/github/cesar1287/jogodavelha/model/GameTest.kt
@@ -1,15 +1,27 @@
 package com.github.cesar1287.jogodavelha.model
 
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 class GameTest {
 
+    private lateinit var game: Game
+    private var player: Player? = null
+    private var anotherPlayer: Player? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        game = Game("Heider", "Android")
+        player = game.player1
+        anotherPlayer = game.player2
+    }
+
+
     @Test
     fun returnTrueIfHasThreeSameHorizontalCellsAtRow1() {
-        val game = Game("Cesar", "1MOB")
-        val player = game.player1
-
         val cell = Cell(player)
         game.cells[0][0] = cell
         game.cells[0][1] = cell
@@ -19,4 +31,45 @@ class GameTest {
 
         assertTrue(hasThreeSameHorizontalCells)
     }
+
+    @Test
+    fun returnTrueIfHasThreeSameHorizontalCellsAtRow2() {
+        val cell = Cell(player)
+        game.cells[1][0] = cell
+        game.cells[1][1] = cell
+        game.cells[1][2] = cell
+
+        val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
+
+        assertTrue(hasThreeSameHorizontalCells)
+    }
+
+    @Test
+    fun returnTrueIfHasThreeSameHorizontalCellsAtRow3() {
+        val cell = Cell(player)
+        game.cells[2][0] = cell
+        game.cells[2][1] = cell
+        game.cells[2][2] = cell
+
+        val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
+
+        assertTrue(hasThreeSameHorizontalCells)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun returnFalseIfDoesNotHaveThreeSameHorizontalCells() {
+        val cell = Cell(player)
+        val anotherCell = Cell(anotherPlayer)
+        game.cells[0][0] = cell
+        game.cells[0][1] = cell
+
+        game.cells[0][2] = anotherCell
+        val hasThreeSameHorizontalCells = game.hasThreeSameHorizontalCells()
+        assertFalse(hasThreeSameHorizontalCells)
+    }
+
+    //00 01 02
+    //10 11 12
+    //20 21 22
 }
