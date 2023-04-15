b1d7c461c82b5146331f35821464c7df06f04cdb
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-04-15 14:22:24 -0300

    finishing the game

--- app/src/main/java/com/github/cesar1287/jogodavelha/ui/GameActivity.kt
+++ app/src/main/java/com/github/cesar1287/jogodavelha/ui/GameActivity.kt
@@ -2,9 +2,17 @@ package com.github.cesar1287.jogodavelha.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.github.cesar1287.jogodavelha.R
 import com.github.cesar1287.jogodavelha.databinding.ActivityGameBinding
+import com.github.cesar1287.jogodavelha.model.Player
 
 class GameActivity : AppCompatActivity() {
+
+    private val viewModel: GameViewModel by lazy {
+        ViewModelProvider(this)[GameViewModel::class.java]
+    }
 
     private val binding: ActivityGameBinding by lazy {
         ActivityGameBinding.inflate(layoutInflater)
@@ -13,16 +21,43 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        promptForPlayers()
     }
 
     // método para setar o jogadores que serão preenchidos pelo dialog
     fun onPlayersSet(player1: String?, player2: String?) {
-
+        initDataBinding(player1, player2)
     }
 
     // Exibe o dialog solicitando o nome dos jogadores
     fun promptForPlayers() {
-
+        val dialog = GameBeginDialog.newInstance(this)
+        dialog.isCancelable = false
+        dialog.show(supportFragmentManager, GAME_BEGIN_DIALOG_TAG)
     }
 
+    private fun initDataBinding(player1: String?, player2: String?) {
+        val activityGameBinding = DataBindingUtil.setContentView<ActivityGameBinding>(this, R.layout.activity_game)
+        viewModel.init(player1, player2)
+        activityGameBinding.gameViewModel = viewModel
+        setUpOnGameEndListener()
+    }
+
+    private fun setUpOnGameEndListener() {
+        viewModel.winner.observe(this) { this.onGameWinnerChanged(it) }
+    }
+
+    private fun onGameWinnerChanged(winner: Player?) {
+        val winnerName = if (winner == null || winner.name.isEmpty()) NO_WINNER else winner.name
+        val dialog = GameEndDialog.newInstance(this, winnerName)
+        dialog.isCancelable = false
+        dialog.show(supportFragmentManager, GAME_END_DIALOG_TAG)
+    }
+
+    companion object {
+
+        private const val GAME_BEGIN_DIALOG_TAG = "game_dialog_tag"
+        private const val GAME_END_DIALOG_TAG = "game_end_dialog_tag"
+        private const val NO_WINNER = "No one"
+    }
 }
--- app/src/main/java/com/github/cesar1287/jogodavelha/ui/GameViewModel.kt
+++ app/src/main/java/com/github/cesar1287/jogodavelha/ui/GameViewModel.kt
@@ -1,4 +1,35 @@
 package com.github.cesar1287.jogodavelha.ui
 
-class GameViewModel {
+import androidx.databinding.ObservableArrayMap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.github.cesar1287.jogodavelha.model.Cell
+import com.github.cesar1287.jogodavelha.model.Game
+import com.github.cesar1287.jogodavelha.model.Player
+
+class GameViewModel : ViewModel() {
+
+    lateinit var cells: ObservableArrayMap<String, String>
+    private lateinit var game: Game
+
+    val winner: LiveData<Player?>
+        get() = game.winner
+
+    fun init(player1: String?, player2: String?) {
+        if (player1 != null && player2 != null) {
+            game = Game(player1, player2)
+            cells = ObservableArrayMap()
+        }
+    }
+
+    fun onClickedCellAt(row: Int, column: Int) {
+        if (game.cells[row][column] == null) {
+            game.cells[row][column] = Cell(game.currentPlayer)
+            cells["$row$column"] = game.currentPlayer?.value
+            if (game.hasGameEnded())
+                game.reset()
+            else
+                game.switchPlayer()
+        }
+    }
 }
